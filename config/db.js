be39e54998748607e41d10962dedc0b3133cf09f
scottfireoved <scottfireoved@gmail.com>
2019-12-02 21:10:25 -0800

    add, update, and delete profiles is now implemented.

--- config/db.js
+++ config/db.js
@@ -3,19 +3,20 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-        });
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
 
-        console.log('MongoDB Connected...');
-    } catch (err) {
-        console.log(err.message);
-        // Exit process with failure
-        process.exit(1);
-    }
-}
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.log(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
